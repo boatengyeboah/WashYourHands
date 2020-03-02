a06f79a3bbd5105b3db77097f80b41b919f2616f
TundeAlao <tunde@cluttr.co.uk>
2020-03-02 05:58:16 -0800

    add text above washing hands

--- app/lib/wash_tab.dart
+++ app/lib/wash_tab.dart
@@ -64,7 +64,22 @@ class _WashTabState extends State<WashTab> {
           onRefresh: fetchData,
           child: ListView(
             children: <Widget>[
-              emojiRow(),
+              RichText(
+                  text: TextSpan(children: [
+                TextSpan(
+                    text:
+                        "The best prevention against the coronavirus is still washing your hands. Tap the button below every time you wash your hands and let's see which country is doing their best.\n"),
+                TextSpan(
+                    text: '#WeAreAllInThisTogether',
+                    style: Theme.of(context).textTheme.body1.copyWith(
+                          color: Color(0xFF62B4FF),
+                          fontWeight: FontWeight.bold,
+                        ))
+              ], style: Theme.of(context).textTheme.body1)),
+              Container(
+                child: emojiRow(),
+                margin: inset3t,
+              ),
               dataWidget(),
               buttonRow(),
               Container(
